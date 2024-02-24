dc63a1b3b958caee200426c2d4994c9990aead4a
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-24 12:54:07 +0200

    Create topBar

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw3.data.ProgressRepository
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
 
@@ -39,6 +43,19 @@ fun CR_3(){
         val progress = ProgressRepository.progresses
         progresslist(progress = progress, contentPadding = it)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Topappbar(modifier: Modifier=Modifier){
+    CenterAlignedTopAppBar(title = {
+        Text(text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.displayLarge,
+            fontSize = 30.sp
+        )
+    },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
