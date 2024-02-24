eeaaba72c60da72aa5f7d1e84b083ec17490ddde
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-24 12:35:12 +0200

    Add more ojects for list with resources

--- app/src/main/java/ua/edu/lntu/cw3/data/progressdatasource.kt
+++ app/src/main/java/ua/edu/lntu/cw3/data/progressdatasource.kt
@@ -8,6 +8,126 @@ object ProgressRepository{
             days = R.string.day1,
             nameres = R.string.name1,
             imageRes = R.drawable.exer_1,
-        )
+        ),
+        progress(
+            days = R.string.day2,
+            nameres = R.string.name2,
+            imageRes = R.drawable.exer_2,
+        ),
+        progress(
+            days = R.string.day3,
+            nameres = R.string.name3,
+            imageRes = R.drawable.exer_3,
+        ),progress(
+            days = R.string.day4,
+            nameres = R.string.name4,
+            imageRes = R.drawable.exer_4,
+        ),progress(
+            days = R.string.day5,
+            nameres = R.string.name5,
+            imageRes = R.drawable.exer_5,
+        ),progress(
+            days = R.string.day6,
+            nameres = R.string.name6,
+            imageRes = R.drawable.exer_6,
+        ),progress(
+            days = R.string.day7,
+            nameres = R.string.name7,
+            imageRes = R.drawable.exer_7,
+        ),progress(
+            days = R.string.day8,
+            nameres = R.string.name8,
+            imageRes = R.drawable.exer_8,
+        ),progress(
+            days = R.string.day9,
+            nameres = R.string.name9,
+            imageRes = R.drawable.exer_9,
+        ),
+        progress(
+            days = R.string.day10,
+            nameres = R.string.name10,
+            imageRes = R.drawable.exer_10,
+        ),progress(
+            days = R.string.day11,
+            nameres = R.string.name11,
+            imageRes = R.drawable.exer_11,
+        ),progress(
+            days = R.string.day12,
+            nameres = R.string.name12,
+            imageRes = R.drawable.exer_12,
+        ),
+        progress(
+            days = R.string.day13,
+            nameres = R.string.name13,
+            imageRes = R.drawable.exer_13,
+        ),progress(
+            days = R.string.day14,
+            nameres = R.string.name14,
+            imageRes = R.drawable.exer_14,
+        ),progress(
+            days = R.string.day15,
+            nameres = R.string.name15,
+            imageRes = R.drawable.exer_15,
+        ),progress(
+            days = R.string.day16,
+            nameres = R.string.name16,
+            imageRes = R.drawable.exer_16,
+        ),progress(
+            days = R.string.day17,
+            nameres = R.string.name17,
+            imageRes = R.drawable.exer_17,
+        ),progress(
+            days = R.string.day18,
+            nameres = R.string.name18,
+            imageRes = R.drawable.exer_18,
+        ),progress(
+            days = R.string.day19,
+            nameres = R.string.name19,
+            imageRes = R.drawable.exer_19,
+        ),progress(
+            days = R.string.day20,
+            nameres = R.string.name20,
+            imageRes = R.drawable.exer_20,
+        ),progress(
+            days = R.string.day21,
+            nameres = R.string.name21,
+            imageRes = R.drawable.exer_21,
+        ),progress(
+            days = R.string.day22,
+            nameres = R.string.name22,
+            imageRes = R.drawable.exer_22,
+        ),progress(
+            days = R.string.day23,
+            nameres = R.string.name23,
+            imageRes = R.drawable.exer_23,
+        ),progress(
+            days = R.string.day24,
+            nameres = R.string.name24,
+            imageRes = R.drawable.exer_24,
+        ),progress(
+            days = R.string.day25,
+            nameres = R.string.name25,
+            imageRes = R.drawable.exer_25,
+        ),progress(
+            days = R.string.day26,
+            nameres = R.string.name26,
+            imageRes = R.drawable.exer_26,
+        ),progress(
+            days = R.string.day27,
+            nameres = R.string.name27,
+            imageRes = R.drawable.exer_27,
+        ),progress(
+            days = R.string.day28,
+            nameres = R.string.name28,
+            imageRes = R.drawable.exer_28,
+        ),progress(
+            days = R.string.day29,
+            nameres = R.string.name29,
+            imageRes = R.drawable.exer_29,
+        ),progress(
+            days = R.string.day30,
+            nameres = R.string.name30,
+            imageRes = R.drawable.exer_30,
+        ),
     )
 }
