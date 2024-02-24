33744064f8dc4034d144ff0b59a7a6e749f3b6c3
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-24 12:32:59 +0200

    Create oject ProgressRepository vith first oject of list

---
+++ app/src/main/java/ua/edu/lntu/cw3/data/progressdatasource.kt
@@ -0,0 +1,13 @@
+package ua.edu.lntu.cw3.data
+
+import ua.edu.lntu.cw3.R
+
+object ProgressRepository{
+    val progresses = listOf(
+        progress(
+            days = R.string.day1,
+            nameres = R.string.name1,
+            imageRes = R.drawable.exer_1,
+        )
+    )
+}
