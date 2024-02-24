5608fa7bdcfbba0e2bf9324849ca25be7ccc9c67
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-24 12:22:56 +0200

    Set typography with imported fonts

--- app/src/main/java/ua/edu/lntu/cw3/ui/theme/Type.kt
+++ app/src/main/java/ua/edu/lntu/cw3/ui/theme/Type.kt
@@ -2,33 +2,34 @@ package ua.edu.lntu.cw3.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ua.edu.lntu.cw3.R
 
 // Set of Material typography styles to start with
+val abril = FontFamily(Font(R.font.abril_fatface_regular))
+
+val montserratregular = FontFamily(Font(R.font.montserrat_regular))
+
+val montserratbold = FontFamily(Font(R.font.montserrat_bold))
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = montserratbold,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 30.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayMedium = TextStyle(
+        fontFamily = montserratregular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = abril,
+        fontWeight = FontWeight.Normal,
+        fontSize = 6.sp
     )
-    */
+
 )
