f9c519c74897486499496b6b9974ccb674d00aab
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-24 12:21:11 +0200

    Use Dark Theme for design

--- app/src/main/java/ua/edu/lntu/cw3/ui/theme/Theme.kt
+++ app/src/main/java/ua/edu/lntu/cw3/ui/theme/Theme.kt
@@ -79,3 +79,20 @@ private val DarkColors = darkColorScheme(
     outlineVariant = md_theme_dark_outlineVariant,
     scrim = md_theme_dark_scrim,
 )
+
+@Composable
+fun IPZ_CR_3Theme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+        LightColors
+    } else {
+        DarkColors
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
